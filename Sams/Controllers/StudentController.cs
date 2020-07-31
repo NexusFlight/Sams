a7f6026fc430df7ff8deeb32e16701f86054b1b4
Andrew Smirthwaite <a.smirthwaite@gmail.com>
2020-07-31 12:52:42 +0100

    Add project files.

---
+++ Sams/Controllers/StudentController.cs
@@ -0,0 +1,57 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Sams.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Sams.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly ILogger<StudentController> logger;
+        private readonly StudentContext sContext;
+        public StudentController(ILogger<StudentController> logger, StudentContext sContext)
+        {
+            this.logger = logger;
+            this.sContext = sContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return sContext.Students
+                .Include(student => student.ContactPreferences)
+                .Include(student => student.ContactDetails)
+                .ThenInclude(contactDetails => contactDetails.Address);
+        }
+
+        [HttpPost]
+        public Student Create([FromBody]Student student)
+        {
+            Console.WriteLine(student.StudentName);
+            var returned = sContext.Add(student);
+            sContext.SaveChanges();
+            return returned.Entity;
+            
+        }
+
+        [HttpDelete]
+        public IActionResult Remove([FromHeader]int id)
+        {
+            var student = sContext.Students.Remove(sContext.Students.Find(id));
+            if(student.Entity != null)
+            {
+                sContext.SaveChanges();
+                return Ok();
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ Sams/Models/StudentContext.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Sams.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options):base(options)
+        {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+                .HasOne(b => b.ContactDetails)
+                .WithOne(i => i.Student);
+
+            modelBuilder.Entity<Student>()
+                .HasOne(b => b.ContactPreferences)
+                .WithOne(i => i.Student);
+
+            modelBuilder.Entity<ContactDetails>()
+                .HasKey(b => b.StudentID);
+
+            modelBuilder.Entity<ContactPreferences>()
+                .HasKey(b => b.StudentID);
+
+            modelBuilder.Entity<ContactDetails>()
+                .HasOne(b => b.Address)
+                .WithOne(i => i.ContactDetails)
+                .HasForeignKey<Address>(i => i.StudentID);
+
+            modelBuilder.Entity<Address>()
+                .HasKey(b => b.StudentID);
+        } 
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
